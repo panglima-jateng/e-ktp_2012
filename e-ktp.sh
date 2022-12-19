27f9825ec5862511814a9989982fa505437f2585
Diki Andreano <92381721+panglima-jateng@users.noreply.github.com>
2022-12-19 21:57:22 +0700

    Add files via upload

---
+++ e-ktp.sh
@@ -0,0 +1,157 @@
+#!/bin/usr/bash
+# code by trio renkarnasi
+# record yakin dek?
+$_mr_virus_spm # defacer
+$_panglima_jateng # programer
+$_prajurit_pajat # script kiides
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+$_hacked_by_trio_renkarnasi_mvs_pajat_prajat
+tampilan(){
+"•==================================================•"
+"  {•} Author : Trio Renkarnasi"
+"  {•} Team   : Mafia Teknologi"
+"•==================================================•"
+}
+#          {https://github.com/MRVIRUSSPM07}
+#          {https://github.com/panglima-jateng}
+#          {https://github.com/prajuritpajat}
+# variabel by trio renkarnasi
+mr_virus_spm=legenda
+Panglima_Jateng=echo
+prajurit_pajat=eror
+lima=ep
+lIma=clone
+jateng_=e
+dg='\e[1;30m' # abu2
+b='\e[0;33m' # kuning
+l='\e[1;31m' # merah
+lc='\e[1;36m' # biru muda
+n='\e[0m' # putih
+lg='\e[1;32m' # hijau
+author=panglima
+pang=sle
+peng=git
+panglima_=-
+rohtua=jateng
+jateng=1
+echo
+echo -e "       "$l"{"$n"•"$l"}"$n" ISI FORMULIR"$l" {"$n"•"$l"}"
+echo -e $l"======================================"$n
+read -p "•> Siapa Nama Kaka : " nama;
+read -p "•> Dari Team Mana  : " team;
+echo -e $b
+sleep 1
+figlet loading
+sleep 1
+loading(){
+load=">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+for i in {1..50}; do
+      echo -ne "\r${load:0:$i}"
+      sleep 0.1
+done
+}
+#       mafia:teknologi:mr:virus:spm
+#       mafia:teknologi:panglima:jateng
+#       mafia:teknologi:prajurit:pajat
+$Panglima_Jateng $panglima_$jateng_ $lg
+loading
+# ulr
+    ${https//www.youtube.com/@panglimajateng8875}
+    $https$panglima_jateng${curl}
+    hukum=ddFtrHYKl9990TryIiHGgsj
+    panglima_jateng=$Panglima_Jateng
+$pang$lima $jateng
+clear # mrvirusspm,panglimajateng,prajuritpajat
+$panglima_jateng -e $l"  _____      _  _______ ____  "
+$panglima_jateng -e $l" | ____|    | |/ /_   _|  _ \ "
+$panglima_jateng -e $l" |  _| _____| ' /  | | | |_) |"
+$panglima_jateng -e $n" | |__|_____| . \  | | |  __/ "
+$panglima_jateng -e $n" |_____|    |_|\_\ |_| |_|    "
+$panglima_jateng -e $l"---------------------------------------------------"
+$panglima_jateng -e $l"{"$n"•"$l"}"$n" Pengguna Script by trio renkarnasi"
+$panglima_jateng -e $n"•"$l"=================================================="$n"•"
+$panglima_jateng -e $l"{"$n"•"$l"}"$n" Nama"$l" :"$n" $nama"
+$panglima_jateng -e $l"{"$n"•"$l"}"$n" Team"$l" :"$n" $team"
+$panglima_jateng -e $n"•"$l"=================================================="$n"•"
+prajurit_pajat=$panglima_jateng
+para=TrIoReNkArNASi_
+pajat=$prajurit_pajat
+anjay=$pajat
+echo
+# daftar isi sebuah script
+    $anjay $panglima_$jateng_ $l"["$n"1"$l"]"$lg"."$n" Install Script"
+    $pajat $panglima_$jateng_ $l"["$n"2"$l"]"$lg"."$n" Login To Script"
+    $pajat $panglima_$jateng_ $l"["$n"3"$l"]"$lg"."$n" order script"
+    $anjay $panglima_$jateng_ $l"["$n"4"$l"]"$lg". Keluar"
+    $anjay $panglima_$jateng_ $n"•"$l"=================================================="$n"•"
+echo
+p=p # pengganti -p
+read $panglima_$p "{•} choose > " dont;
+# install script
+if [[ $dont == "1" ]]; then
+  echo "lanjut { y / n }"
+  read lanj;
+  if [[ $lanj == "y" ]] || [[ $lanj == "Y" ]]; then
+  loading
+         sleep 1
+         clear
+	 figlet installting
+         sleep 3
+         juta=hub
+         pertama=$peng
+ 	 kedua=$lIma
+  echo $panglima_$jateng_ "•==================================================•"
+  $pertama $kedua https://$pertama$juta.com/prajurit-pajat/$para$hukum
+  $_____________panglima_jateng____________
+  clear
+     figlet selesai
+     echo "silahkan masuk lalu pilih nomor 2"
+     sleep 5
+  bash e-ktp.sh
+  else
+      bash e-ktp.sh
+  fi
+# login to script
+elif [[ $dont == "2" ]]; then
+   echo "apakah anda sudah menginstall script? ( y / n )"
+   read inss;
+   if [[ $inss == "y" ]] || [[ $inss == "Y" ]]; then
+   cd $para$hukum
+   bash InStAl_DuLugennnnjkkk.sh
+   else
+       bash e-ktp.sh
+   fi
+elif [[ $dont == "3" ]]; then
+   echo
+   echo -e $l"{"$n"+"$l"}"$n" Order script"
+   echo -e $l"------------------------------------------------"
+   echo -e $l"{"$n"•"$l"}"$n" balik nama"$l" :"$n" 1jt bonus file data e-ktp"
+   echo -e $l"{"$n"•"$l"}"$n" copyright "$l" :"$n" 50k / 20 data e-ktp"
+   echo -e $l"{"$n"•"$l"}"$n" transaksi "$l" :"$n" dana/bri"
+   echo -e $l"{"$n"•"$l"}"$n" nomor     "$l" :"$n" +62 882-1217-1828"
+   echo -e $l"------------------------------------------------"
+# keluar
+elif [[ $dont == "4" ]]; then
+echo
+echo "================================================="
+echo "{•} terimakasih telah menggunakan script saya {•}"
+echo "================================================="
+else
+    echo "masukan perintah dengan benar"
+    sleep 3
+    bash e-ktp.sh
+fi
